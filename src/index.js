5315d384e0ff06ec6f761b4417c8123471d9e7d7
Dmytro Hanych <Hanych_IT@ukr.net>
2022-12-11 22:52:57 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styleConfig/theme';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
