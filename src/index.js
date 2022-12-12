774d0b7bc02e7d9ad35da5b450f24ae9efe370ea
Dmytro Hanych <Hanych_IT@ukr.net>
2022-12-12 22:11:04 +0200

    fixed spinner

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from 'styleConfig/theme';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
